7a1e22c5d0c00f5eeef2c54e8c8bed2d76ed2289
Sabmus <abiatar.ms@gmail.com>
2024-01-24 20:03:00 -0300

    adding combobox component

--- components/ui/Combobox.tsx
+++ components/ui/Combobox.tsx
@@ -1,14 +1,51 @@
+'use client';
+
+import { ChangeEvent, useState, useRef } from 'react';
+import useOnClickOutside from '@/hooks/useOnClickOutside';
+
 type TDataArray = string[];
 
 const Combobox = ({ dataArray }: { dataArray: TDataArray }) => {
+  const [active, setActive] = useState(false);
+  const [query, setQuery] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOnClickOutside(inputRef, () => setActive(false));
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleFocus = () => {
+    setActive(true);
+    console.log('focus');
+  };
+
+  const filteredData =
+    query === '' ? dataArray : dataArray.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+
   return (
-    <select name="category" id="category">
-      {dataArray.map((item, idx) => (
-        <option key={idx} value={item}>
-          {item}
-        </option>
-      ))}
-    </select>
+    <div className="relative" ref={inputRef}>
+      <input
+        type="text"
+        name="name"
+        id="id"
+        className="w-full"
+        placeholder="type something..."
+        onChange={e => handleInputChange(e)}
+        onFocus={handleFocus}
+        value={query}
+        autoComplete="off"
+      />
+      <div className="absolute bg-blue-400 w-full top-7">
+        {active &&
+          filteredData.map(item => (
+            <div key={item} className="">
+              {item}
+            </div>
+          ))}
+      </div>
+    </div>
   );
 };
 
