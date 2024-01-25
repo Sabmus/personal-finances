0fcb5e84806978f2e22cb8621d6815e075a8c6b3
sabmus <abiatar.ms@gmail.com>
2024-01-24 22:19:55 -0300

    feat: create combobox component

--- components/ui/Combobox.tsx
+++ components/ui/Combobox.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { ChangeEvent, useState, useRef } from 'react';
+import { ChangeEvent, useState, useRef, useEffect, MouseEvent, forwardRef, HTMLProps } from 'react';
 import useOnClickOutside from '@/hooks/useOnClickOutside';
 
 type TDataArray = string[];
+type InputProps = HTMLProps<HTMLInputElement> & { dataArray: TDataArray };
 
-const Combobox = ({ dataArray }: { dataArray: TDataArray }) => {
+const Combobox = forwardRef<HTMLInputElement, InputProps>(({ dataArray, ...props }, ref) => {
+  const { id, name, className, placeholder } = props;
+
   const [active, setActive] = useState(false);
   const [query, setQuery] = useState('');
   const inputRef = useRef<HTMLInputElement>(null);
@@ -16,37 +19,68 @@ const Combobox = ({ dataArray }: { dataArray: TDataArray }) => {
     setQuery(e.target.value);
   };
 
-  const handleFocus = () => {
-    setActive(true);
-    console.log('focus');
+  const handleItemClick = (e: MouseEvent<HTMLDivElement>) => {
+    setQuery(e.currentTarget.innerText);
+    setActive(false);
   };
 
   const filteredData =
     query === '' ? dataArray : dataArray.filter(item => item.toLowerCase().includes(query.toLowerCase()));
 
+  const KEY_CODES = {
+    ESCAPE: 'Escape',
+    ESCAPE_IE11: 'Esc',
+    TAB: 'Tab',
+  };
+
+  const keyDownHandler = (e: { key: any }) => {
+    switch (e.key) {
+      case KEY_CODES.TAB:
+      case KEY_CODES.ESCAPE:
+      case KEY_CODES.ESCAPE_IE11: {
+        setActive(false);
+        document.getElementById(`${id}`)?.blur();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="relative" ref={inputRef}>
       <input
+        ref={ref}
+        id={id}
+        name={name}
+        className={className}
+        placeholder={placeholder}
         type="text"
-        name="name"
-        id="id"
-        className="w-full"
-        placeholder="type something..."
         onChange={e => handleInputChange(e)}
-        onFocus={handleFocus}
+        onFocus={() => setActive(true)}
         value={query}
         autoComplete="off"
       />
-      <div className="absolute bg-blue-400 w-full top-7">
+      <div className="absolute bg-blue-400 w-full top-7 z-10">
         {active &&
           filteredData.map(item => (
-            <div key={item} className="">
+            <div key={item} className="hover:bg-green-400" onClick={e => handleItemClick(e)}>
               {item}
             </div>
           ))}
       </div>
     </div>
   );
-};
+});
+Combobox.displayName = 'Combobox';
 
 export default Combobox;
