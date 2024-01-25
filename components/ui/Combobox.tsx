7b24deda92de2e0787c81670e18162f09dc218ee
sabmus <abiatar.ms@gmail.com>
2024-01-24 23:19:31 -0300

    feat: added movement with up and down arrow to combobox

--- components/ui/Combobox.tsx
+++ components/ui/Combobox.tsx
@@ -9,19 +9,23 @@ type InputProps = HTMLProps<HTMLInputElement> & { dataArray: TDataArray };
 const Combobox = forwardRef<HTMLInputElement, InputProps>(({ dataArray, ...props }, ref) => {
   const { id, name, className, placeholder } = props;
 
-  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
   const [query, setQuery] = useState('');
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [active, setactive] = useState(-1);
 
-  useOnClickOutside(inputRef, () => setActive(false));
+  const inputRef = useRef<HTMLInputElement>(null);
+  useOnClickOutside(inputRef, () => {
+    setOpen(false);
+    setactive(-1);
+  });
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
-  const handleItemClick = (e: MouseEvent<HTMLDivElement>) => {
+  const handleItemClick = (e: MouseEvent<HTMLLIElement>) => {
     setQuery(e.currentTarget.innerText);
-    setActive(false);
+    setOpen(false);
   };
 
   const filteredData =
@@ -31,15 +35,43 @@ const Combobox = forwardRef<HTMLInputElement, InputProps>(({ dataArray, ...props
     ESCAPE: 'Escape',
     ESCAPE_IE11: 'Esc',
     TAB: 'Tab',
+    ARROW_UP: 'ArrowUp',
+    ARROW_UP_IE11: 'Up',
+    ARROW_DOWN: 'ArrowDown',
+    ARROW_DOWN_IE11: 'Down',
+    ENTER: 'Enter',
   };
 
-  const keyDownHandler = (e: { key: any }) => {
+  const keyDownHandler = (e: any) => {
     switch (e.key) {
       case KEY_CODES.TAB:
       case KEY_CODES.ESCAPE:
       case KEY_CODES.ESCAPE_IE11: {
-        setActive(false);
+        setOpen(false);
+        setactive(-1);
         document.getElementById(`${id}`)?.blur();
+        break;
+      }
+      case KEY_CODES.ARROW_DOWN_IE11:
+      case KEY_CODES.ARROW_DOWN: {
+        if (open && active < filteredData.length - 1) {
+          setactive(prev => prev + 1);
+        }
+        break;
+      }
+      case KEY_CODES.ARROW_UP_IE11:
+      case KEY_CODES.ARROW_UP: {
+        if (open && active > 0) {
+          setactive(prev => prev - 1);
+        }
+        break;
+      }
+      case KEY_CODES.ENTER: {
+        if (open && active > -1) {
+          e.preventDefault();
+          setQuery(filteredData[active]);
+          setOpen(false);
+        }
         break;
       }
       default: {
@@ -66,17 +98,26 @@ const Combobox = forwardRef<HTMLInputElement, InputProps>(({ dataArray, ...props
         placeholder={placeholder}
         type="text"
         onChange={e => handleInputChange(e)}
-        onFocus={() => setActive(true)}
+        onFocus={() => setOpen(true)}
+        onKeyDown={e => keyDownHandler(e)}
         value={query}
         autoComplete="off"
       />
       <div className="absolute bg-blue-400 w-full top-7 z-10">
-        {active &&
-          filteredData.map(item => (
-            <div key={item} className="hover:bg-green-400" onClick={e => handleItemClick(e)}>
-              {item}
-            </div>
-          ))}
+        {open && (
+          <ul>
+            {filteredData.map((item, idx) => (
+              <li
+                key={item}
+                className={`hover:bg-green-400 ${active === idx ? 'bg-green-400' : null}`}
+                onClick={e => handleItemClick(e)}
+                onMouseEnter={() => setactive(-1)}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
