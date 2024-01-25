5ce2c0ff2b372d288858975e985cb379304f2b59
sabmus <abiatar.ms@gmail.com>
2024-01-24 23:19:47 -0300

    other: change in mock  item names

--- app/dashboard/payment/create/page.tsx
+++ app/dashboard/payment/create/page.tsx
@@ -1,8 +1,8 @@
 import { CreatePaymentForm } from '@/components';
 
 const CreatePayment = () => {
-  const categories = ['item1', 'item2', 'item3'];
-  const paymentMethods = ['item1', 'item2', 'item3'];
+  const categories = ['category 1', 'category 2', 'category 3'];
+  const paymentMethods = ['payment 1', 'payment 2', 'payment 3'];
 
   return (
     <div className="h-full border-test">
