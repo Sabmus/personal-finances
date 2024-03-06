542ac22d5e4bb260ad5015bcef9fa03c9c005ec7
Sabmus <abiatar.ms@gmail.com>
2024-03-06 19:56:56 -0300

    added chart.js

--- components/dashboard/LineChart.tsx
+++ components/dashboard/LineChart.tsx
@@ -1,5 +1,53 @@
+'use client';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    labels: {},
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => 1),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
+
 const LineChart = () => {
-  return <div>LineChart</div>;
+  return (
+    <div className="border-test h-full">
+      <Line options={options} data={data} />
+    </div>
+  );
 };
 
 export default LineChart;
