6ec2fff36704014e65c3654a1a09f2d5530ae2e6
Sabmus <abiatar.ms@gmail.com>
2024-03-06 17:37:56 -0300

    style: center text

--- components/dashboard/AmountCard.tsx
+++ components/dashboard/AmountCard.tsx
@@ -13,9 +13,8 @@ const AmountCard = ({ title, amount, info }: IAmountCardProps) => {
         <h6 className="text-foreground/50">{title}</h6>
         <span className="text-foreground/50">icon</span>
       </div>
-      <div className="flex justify-center gap-1 items-baseline">
+      <div className="text-center">
         <h1 className="text-accent text-header-clamp">{toCLP(amount)}</h1>
-        <span className="text-xs">CLP</span>
       </div>
       <div className="w-2/3 mx-auto text-xs text-center">
         <span className="text-foreground/50">10% of salary</span>
